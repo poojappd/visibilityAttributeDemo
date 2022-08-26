b4f1c770c3bf4b21bee3140306327cd4b0c28a21
poojappd <poojapriyadarshnips@gmail.com>
2022-08-26 17:38:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/visibilityattributedemo/ConstraintWidthDefault.kt
@@ -0,0 +1,11 @@
+package com.example.visibilityattributedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintWidthDefault : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_width_default)
+    }
+}
---
+++ app/src/main/java/com/example/visibilityattributedemo/barriersAndGuidelines.kt
@@ -0,0 +1,11 @@
+package com.example.visibilityattributedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class barriersAndGuidelines : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barriers_and_guidelines)
+    }
+}
